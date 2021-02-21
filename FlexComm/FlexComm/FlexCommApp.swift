58eff37a463e76b6022907ea30afb7a8e46e96ff
emkao <emilykao100@gmail.com>
2021-02-21 11:40:27 -0500

    initial commit

---
+++ FlexComm/FlexComm/FlexCommApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlexCommApp.swift
+//  FlexComm
+//
+//  Created by emily kao on 2/21/21.
+//
+
+import SwiftUI
+
+@main
+struct FlexCommApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
