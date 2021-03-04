6c3358aaebb6f27e1dc6ed57ba9b050d54efd57c
emkao <emilykao100@gmail.com>
2021-03-04 10:30:29 -0500

    added CustomButton struct and renamed yesNoView to OptionsView

---
+++ FlexComm/FlexComm/CustomButton.swift
@@ -0,0 +1,8 @@
+//
+//  CustomButton.swift
+//  FlexComm
+//
+//  Created by emily kao on 3/4/21.
+//
+
+import Foundation
---
+++ FlexComm/FlexComm/OptionsView.swift
@@ -0,0 +1,20 @@
+//
+//  OptionsView.swift
+//  FlexComm
+//
+//  Created by emily kao on 3/4/21.
+//
+
+import SwiftUI
+
+struct OptionsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct OptionsView_Previews: PreviewProvider {
+    static var previews: some View {
+        OptionsView()
+    }
+}
